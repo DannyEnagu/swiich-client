6a3e0c1756c5272f9db8bb372d1b73f3b5a1a312
danny <danielenagu@gmail.com>
2024-08-26 17:09:11 +0100

    Refactor setting component imports

--- src/app/dashboard/[orgID]/settings/Components/Navigation/Navigation.tsx
+++ src/app/dashboard/[orgID]/settings/Components/Navigation/Navigation.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPen,
@@ -11,65 +12,85 @@ import NavItem from "@/components/dashboard/Nav/NavItem";
 import NavList from "@/components/dashboard/Nav/NavList";
 import styles from "./Navigation.module.css";
 import NavBar from "@/components/dashboard/Nav/NavBar";
+import { useState } from "react";
 
-export default function Navigation() {
+interface SettingsProps {
+  onChange: (setting: string) => void;
+}
+
+export default function Navigation({ onChange }: SettingsProps) {
+ const [settings, setSettings] = useState([
+  {
+    icon: faPen,
+    label: "General",
+    isActive: true,
+  },
+  {
+    icon: faUser,
+    label: "Account",
+    isActive: false,
+  },
+  {
+    icon: faBell,
+    label: "Notification",
+    isActive: false,
+  },
+  {
+    icon: faMicrophone,
+    label: "Audio",
+    isActive: false,
+  },
+  {
+    icon: faVideo,
+    label: "Video",
+    isActive: false,
+  },
+  {
+    icon: faCog,
+    label: "Privacy",
+    isActive: false,
+  }
+]);
+
+  const changeSetting = (setting: typeof settings[0]) => {
+    setSettings((prevSettings) =>
+      prevSettings.map((prevSetting) => {
+        if (prevSetting.label === setting.label) {
+          return {
+            ...prevSetting,
+            isActive: true,
+          };
+        }
+        return {
+          ...prevSetting,
+          isActive: false,
+        };
+      })
+    );
+    onChange(setting.label);
+  };
+
   return (
     <NavBar>
       <div className={styles.header}>
         <h2>Settings</h2>
       </div>
       <NavList>
-        <NavItem>
-          <button className={`btn
-            ${styles.settingsButton}
-            ${styles.settingsButtonActive}
-          `}>
-            <FontAwesomeIcon icon={faPen} className={styles.settingsIcon} />
-            <span className={styles.settingsLable}>General</span>
-          </button>
-        </NavItem>
-        <NavItem>
-          <button className={`btn
-            ${styles.settingsButton}
-          `}>
-              <FontAwesomeIcon icon={faUser} className={styles.settingsIcon} />
-              <span className={styles.settingsLable}>Account</span>
-          </button>
-        </NavItem>
-        <NavItem>
-          <button className={`btn
-            ${styles.settingsButton}
-          `}>
-            <FontAwesomeIcon icon={faBell} className={styles.settingsIcon} />
-            <span className={styles.settingsLable}>
-              Notification
-            </span>
-          </button>
-        </NavItem>
-        <NavItem>
-          <button className={`btn
-              ${styles.settingsButton}
-            `}>
-            <FontAwesomeIcon icon={faMicrophone} className={styles.settingsIcon} />
-            <span className={styles.settingsLable}>Audio</span>
-          </button>
-        </NavItem>
-        <NavItem>
-          <button className={`btn
-              ${styles.settingsButton}
-            `}>
-            <FontAwesomeIcon icon={faVideo} className={styles.settingsIcon} />
-            <span className={styles.settingsLable}>Video</span>
-          </button>
-        </NavItem>
-        <NavItem>
-          <button className={`btn
-              ${styles.settingsButton}
-            `}>
-            <FontAwesomeIcon icon={faCog} className={styles.settingsIcon} />
-            <span className={styles.settingsLable}>Privacy</span>
-          </button>
-        </NavItem>
+        {settings.map((setting, index) => (
+          <NavItem key={index}>
+            <button className={`btn
+                ${styles.settingsButton}
+                ${setting.isActive ? styles.settingsButtonActive : ""}
+              `}
+              onClick={() => changeSetting(setting)}
+            >
+              <FontAwesomeIcon icon={setting.icon} className={styles.settingsIcon} />
+              <span className={styles.settingsLable}>
+                {setting.label}
+              </span>
+            </button>
+          </NavItem>
+        ))}
       </NavList>
     </NavBar>
   );
--- src/app/dashboard/[orgID]/settings/page.tsx
+++ src/app/dashboard/[orgID]/settings/page.tsx
@@ -1,17 +1,24 @@
+'use client';
+import { useState } from "react";
 import withContentWrapper from "@/components/dashboard/WithContentWrapper";
 import Main from "@/components/dashboard/MainContainer";
 import Header from "@/components/dashboard/Header/Header";
-import { General, Navigation } from "./Components";
+import { Navigation } from "./Components";
 
-const GeneralWrapper = withContentWrapper(General);
+const renderComponent = (component: string) => {
+  const Components = require(`./Components/`);
+  const Component = Components[component];
+  return Component ? <Component /> : null;
+};
 
 export default function Page() {
+  const [activeComponent, setActiveComponent] = useState("General");
   return (
     <div className="dd-content">
-      <Navigation />
+      <Navigation onChange={(setting) => setActiveComponent(setting)} />
       <Header />
       <Main>
-        <GeneralWrapper />
+        {renderComponent(activeComponent)}
       </Main>
     </div>
   );
